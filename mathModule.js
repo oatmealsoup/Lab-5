69600f6adafb75687ae6be1da1e3494100dc031d
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-18 23:44:47 -0400

    Update mathModule.js

--- mathModule.js
+++ mathModule.js
@@ -1,3 +1,4 @@
 // mathModule.js
-export function add(a, b) {return a + b;}
-export function subtract(a, b) {return a - b;}
+export function add(a, b) {return a+b;}
+export function subtract(a, b) {return a-b;}
+export default function multiply(a, b) {return a*b;}
