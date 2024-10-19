51334752effa057bbb318b8c910f2fed80c441c5
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-19 01:23:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ console.log('Max: ', findMax([1, 2, 3, 4, 5]));  // Test findMax
 console.log('Reversed: ', reverseArray([1, 2, 3]));  // Test reverseArray
 addAndLogUpper(10, 20);  // Test the new function
 console.log([1,2,3,4,5,6,7,8,9,0]);
-max=findmax([1,2,3,4,5,6,7,8,9,0]);
-console.log(max);
+console.log(findmax([1,2,3,4,5,6,7,8,9,0]));
 console.log('ten max:', multiply(max, 10));
 addAndLogUpper(0, multiply(max, 10));
 
