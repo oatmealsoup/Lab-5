684f6e1a42a7075f02e30d8579e20be355590a41
oatmealsoup <116693628+oatmealsoup@users.noreply.github.com>
2024-10-19 00:07:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // app.js
 import multiply, { add, subtract } from './mathModule.js';
 import { toUpperCase, toLowerCase } from './stringModule.js';
+import { findMax, reverseArray } from './arrayModule.js';
  
 // Call the functions and log results (your choice of arguments)
 console.log('Add: ', add(5, 3));  // Test your add function
 console.log('Multiply: ', multiply(4, 5));  // Test the default export
 console.log('Uppercase: ', toUpperCase('hello'));  // Test string manipulation
+console.log('Max: ', findMax([1, 2, 3, 4, 5]));  // Test findMax
+console.log('Reversed: ', reverseArray([1, 2, 3]));  // Test reverseArray
